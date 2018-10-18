cae6cbc84b713cf1144a2abb076f4165fce24bd1
Raymond Loman <raymond@carpago.nl>
2018-10-18 21:11:49 +0200

    Nice p

---
+++ src/main/java/yc/klm/booking/domain/Order.java
@@ -0,0 +1,20 @@
+package yc.klm.booking.domain;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+public class Order implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    private Account account;
+
+
+    private Flight flight;
+
+
+}
