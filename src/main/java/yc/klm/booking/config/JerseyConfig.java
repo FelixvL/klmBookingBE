032154e446b8452bdc10918b92b628c76cfa098b
Raymond Loman <raymond@carpago.nl>
2018-10-19 07:58:15 +0200

    Remove airtrafficendpoint

--- src/main/java/yc/klm/booking/config/JerseyConfig.java
+++ src/main/java/yc/klm/booking/config/JerseyConfig.java
@@ -4,7 +4,6 @@ package yc.klm.booking.config;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.stereotype.Component;
 import yc.klm.booking.api.AccountEndpoint;
-import yc.klm.booking.api.AirTrafficEndpoint;
 import yc.klm.booking.api.OrderEndpoint;
 
 import javax.ws.rs.ApplicationPath;
@@ -14,7 +13,6 @@ import javax.ws.rs.ApplicationPath;
 @ApplicationPath("/api")
 public class JerseyConfig extends ResourceConfig {
     public JerseyConfig(){
-        register(AirTrafficEndpoint.class);
         register(AccountEndpoint.class);
         register(OrderEndpoint.class);
 
